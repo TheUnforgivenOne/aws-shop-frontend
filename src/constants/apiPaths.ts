61d86b16308deb1652a2fae69d24a6cf74ee8d05
TheUnforgivenOne <vladpotapow@yandex.ru>
2024-04-23 18:37:52 +0200

    Added alerts on 4xx errors while importing

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://ma2vf10m84.execute-api.eu-west-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://ejyo0nig7c.execute-api.eu-west-1.amazonaws.com",
+  import: "https://jq71gt7425.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
